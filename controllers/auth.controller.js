03a4cd9e6b4895f76d3974331311584f64212121
Muhammed Farzin Farooque T K <mfarooquetk@gmail.com>
2025-06-23 01:29:21 +0530

    Refactor user registration and login logic for improved readability and consistency

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,88 +1,87 @@
-import { User } from "../models/index.js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import "dotenv/config";
+import { User } from '../models/index.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import 'dotenv/config'
 
 export const register = async (req, res) => {
   try {
-    const { name, email, phone_number, password } = req.body;
-    const existingUser = await User.findOne({ where: { email } });
-    if (existingUser.email === email) {
-      return res.status(400).json({ message: "Email already registered." });
+    const { name, email, phone_number, password } = req.body
+    const emailExists = await User.findOne({ where: { email } })
+    if (emailExists) {
+      return res.status(400).json({ message: 'Email already registered.' })
+    }
+    const phoneExists = await User.findOne({ where: { phone_number } })
+    if (phoneExists) {
+      return res.status(400).json({ message: 'Phone number already registered.' })
     }
 
-    if (existingUser.phone_number === phone_number) {
-      return res.status(400).json({ message: "Phone number already registered." });
-    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await User.create({ name, email, phone_number, password: hashedPassword })
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({ name, email, phone_number, password: hashedPassword });
-
-    res.status(201).json({ message: "User created succesfully." });
+    res.status(201).json({ message: 'User created succesfully.' })
   } catch (err) {
-    console.log("Registration failed.", err);
-    res.status(500).json({ message: "Internal server error." });
+    console.log('Registration failed.', err)
+    res.status(500).json({ message: 'Internal server error.' })
   }
-};
+}
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password." });
+      return res.status(401).json({ message: 'Invalid email or password.' })
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-      return res
-        .status(401)
-        .json({ message: "Invalid password. Please try again." });
+      return res.status(401).json({ message: 'Invalid password. Please try again.' })
     }
 
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    })
 
-    res.status(200).cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000,
-    }).json({message:"Login successfull."});
+    res
+      .status(200)
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 24 * 60 * 60 * 1000
+      })
+      .json({ message: 'Login successfull.' })
   } catch (err) {
-    console.log("Login failed",err)
-    res.status(500).json({ message: "Internal server error." });
+    console.log('Login failed', err)
+    res.status(500).json({ message: 'Internal server error.' })
   }
-};
+}
 export const logout = async (req, res) => {
-  res.status(200).clearCookie("token").json({ message: "Logged out successfully" });
-};
+  res.status(200).clearCookie('token').json({ message: 'Logged out successfully' })
+}
 
 export const deleteUser = async (req, res) => {
   try {
-    const id = req.user.id; 
-    const { confirmationText } = req.body; 
+    const id = req.user.id
+    const { confirmationText } = req.body
 
-    if (confirmationText?.toLowerCase() !== "delete my account") {
-      return res.status(400).json({ 
-        message: "Please type 'delete my account' to confirm deletion." 
-      });
+    if (confirmationText?.toLowerCase() !== 'delete my account') {
+      return res.status(400).json({
+        message: "Please type 'delete my account' to confirm deletion."
+      })
     }
 
-    const deleted = await User.destroy({ where: { id } });
+    const deleted = await User.destroy({ where: { id } })
 
     if (deleted) {
       res.clearCookie('token')
-      return res.status(200).json({ message: "User deleted successfully." });
+      return res.status(200).json({ message: 'User deleted successfully.' })
     } else {
-      return res.status(404).json({ message: "User not found." });
+      return res.status(404).json({ message: 'User not found.' })
     }
   } catch (error) {
-    console.error("Error deleting user:", error);
-    res.status(500).json({ message: "Internal server error." });
+    console.error('Error deleting user:', error)
+    res.status(500).json({ message: 'Internal server error.' })
   }
-};
+}
